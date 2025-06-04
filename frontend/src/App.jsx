8f59e63bcb9db59c7b79601d68f2aa8a9d251e20
tirthkheni <202301007@daiict.ac.in>
2025-06-05 01:55:00 +0530

    add theme to clerkAuth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,26 @@ import IndexPage from './pages/Index'
 import SignInPage from './pages/SignIn'
 import SignUpPage from './pages/SignUp'
 import DashboardPage from './pages/Dashboard'
+import Profile from './pages/Profile'
+
+// This is alternate method
+// const router = createBrowserRouter([
+//   {
+//     element: <RootLayout />,
+//     children: [
+//       { path: "/", element: <IndexPage /> },
+//       { path: "/sign-in/*", element: <SignInPage /> },
+//       { path: "/sign-up/*", element: <SignUpPage /> },
+//       {
+//         element: <DashboardLayout />,
+//         path: "dashboard",
+//         children: [
+//           { path: "/dashboard", element: <DashboardPage /> }
+//         ]
+//       }
+//     ]
+//   }
+// ])
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -18,6 +38,7 @@ const router = createBrowserRouter(
       <Route index element={<IndexPage />} />
       <Route path='/sign-in/*' element={<SignInPage />} />
       <Route path='/sign-up/*' element={<SignUpPage />} />
+      <Route path='/profile' element={<Profile />} />
       <Route path='/dashboard' element={<DashboardLayout />}>
         <Route index element={<DashboardPage />}/>
       </Route>
