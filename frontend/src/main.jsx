7eab7a15436941509b21048d40ba631027a40d2a
tirthkheni <202301007@daiict.ac.in>
2025-06-05 02:00:00 +0530

    add landing page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
-import { dark } from '@clerk/themes';
+import {dark} from '@clerk/themes';
 
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ClerkProvider
     publishableKey={PUBLISHABLE_KEY}
-    appearance={{baseTheme: dark }}
+    appearance={{baseTheme: dark}}
     >
       <App />
     </ClerkProvider>
