64970236835bab32d39c7522c49524b4e7eb82a2
tirthkheni <202301007@daiict.ac.in>
2025-06-20 16:23:00 +0530

    add comments with nesting

--- frontend/src/pages/BlogDetailPage.jsx
+++ frontend/src/pages/BlogDetailPage.jsx
@@ -1,42 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
-import { FiThumbsUp, FiMessageCircle } from "react-icons/fi";
+import { FiMessageCircle, FiBookmark, FiArrowUp, FiArrowDown } from "react-icons/fi";
 import { IoArrowBack } from "react-icons/io5";
-import { FaEdit, FaEye, FaThumbsDown } from "react-icons/fa";
-import {
-  TiArrowUpOutline,
-  TiArrowDownOutline,
-  TiArrowUp,
-  TiArrowDown,
-} from "react-icons/ti";
+import { FaEdit, FaEye } from "react-icons/fa";
 import Header from "../components/Header";
 import useFetchBlogPostbyId from "../hooks/useFetchBlogPostbyId";
 import useAddCommentToBlogPost from "../hooks/useAddCommentToBlogPost";
 import useUpvoteBlogPost from "../hooks/useUpvoteBlogPost";
 import useDownvoteBlogPost from "../hooks/useDownvoteBlogPost";
+import useUpvoteComment from "../hooks/useUpvoteComment";
+import useDownvoteComment from "../hooks/useDownvoteComment";
 import { useAuth } from "@clerk/clerk-react";
 import useFetchUserById from "../hooks/useFetchUserById";
 import useIncrementBlogPostView from "../hooks/useIncrementBlogPostView";
+import useFavoriteBlogPost from "../hooks/useFavoriteBlogPost";
+import useUnfavoriteBlogPost from "../hooks/useUnfavoriteBlogPost";
 import formatDate from "../utils/formatDate";
 
 const BlogDetailPage = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const { blogPost, loading: postLoading } = useFetchBlogPostbyId(id);
+  const [sortMethod, setSortMethod] = useState("recent");
+
+  const { blogPost, loading: postLoading } = useFetchBlogPostbyId(id, sortMethod);
   const { addComment } = useAddCommentToBlogPost(id);
   const { upvote } = useUpvoteBlogPost(id);
   const { downvote } = useDownvoteBlogPost(id);
   const { incrementView } = useIncrementBlogPostView(id);
+  const { favorite } = useFavoriteBlogPost(id);
+  const { unfavorite } = useUnfavoriteBlogPost(id);
+  const { upvoteComment } = useUpvoteComment(id);
+  const { downvoteComment } = useDownvoteComment(id);
   const [commentContent, setCommentContent] = useState("");
+  const [replyContent, setReplyContent] = useState("");
+  const [replyingTo, setReplyingTo] = useState(null);
   const { userId, isLoaded: authLoaded } = useAuth();
   const { user, loading: userLoading } = useFetchUserById(userId);
   const [comments, setComments] = useState([]);
   const [upvoteCount, setUpvoteCount] = useState(0);
   const [downvoteCount, setDownvoteCount] = useState(0);
   const [viewCount, setViewCount] = useState(0);
-  const [hasviewed, setHasViewed] = useState(true);
+  const [hasViewed, setHasViewed] = useState(true);
   const [hasUpvoted, setHasUpvoted] = useState(false);
   const [hasDownvoted, setHasDownvoted] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
 
   useEffect(() => {
     if (!postLoading && blogPost) {
@@ -49,11 +56,13 @@ const BlogDetailPage = () => {
       setDownvoteCount(blogPost.downvotes.length);
       if (!userLoading && user) {
         setHasViewed(blogPost.views.includes(user._id));
-        if (!hasviewed) {
+        if (!hasViewed) {
           incrementView(user._id).then(() => {
             setHasViewed(true);
-            setViewCount(viewCount + 1);})
+            setViewCount(viewCount + 1);
+          });
         }
+        setIsFavorite(user.favorites.some((favPost) => favPost._id === id));
         setHasUpvoted(blogPost.upvotes.includes(user._id));
         setHasDownvoted(blogPost.downvotes.includes(user._id));
       }
@@ -63,7 +72,7 @@ const BlogDetailPage = () => {
   if (postLoading || userLoading) {
     return <div>Loading...</div>;
   }
-
+console.log(comments)
   if (!blogPost.visibility && blogPost.author.clerkId !== userId) {
     return (
       <div className="bg-primary text-primary_text p-4 rounded mb-6">
@@ -84,6 +93,25 @@ const BlogDetailPage = () => {
         setComments([...comments, newComment]);
       }
       setCommentContent("");
+    }
+  };
+
+  const handleReplySubmit = async (e, parentId) => {
+    e.preventDefault();
+    if (user && user._id && parentId) {
+      const newReply = await addComment(replyContent, user._id, parentId);
+      if (newReply) {
+        setComments(
+          comments.map((comment) => {
+            if (comment._id === parentId) {
+              return { ...comment, replies: [...comment.replies, newReply] };
+            }
+            return comment;
+          })
+        );
+        setReplyingTo(null);
+        setReplyContent("");
+      }
     }
   };
 
@@ -111,6 +139,150 @@ const BlogDetailPage = () => {
     }
   };
 
+  const handleFavorite = async () => {
+    if (user && user._id && !isFavorite) {
+      await favorite(user._id);
+      setIsFavorite(true);
+    }
+  };
+
+  const handleUnfavorite = async () => {
+    if (user && user._id && isFavorite) {
+      await unfavorite(user._id);
+      setIsFavorite(false);
+    }
+  };
+
+  const handleReplyClick = (comment) => {
+    setReplyingTo(comment._id )
+    setReplyContent(`@${comment.author.name}`)
+ };
+ const handleCommentUpvote = async (commentId, parentId, hasUpvoted) => {
+  if (user && user._id && !hasUpvoted) {
+    await upvoteComment(commentId, user._id);
+    setComments(
+      comments.map((comment) => {
+        if (comment._id === commentId) {
+          return {
+            ...comment,
+            upvotes: [...comment.upvotes, user._id],
+            downvotes: comment.downvotes.filter((id) => id !== user._id),
+          };
+        } else if (comment._id === parentId) {
+          return {
+            ...comment,
+            replies: comment.replies.map((reply) => {
+              if (reply._id === commentId) {
+                return {
+                  ...reply,
+                  upvotes: [...reply.upvotes, user._id],
+                  downvotes: reply.downvotes.filter((id) => id !== user._id),
+                };
+              }
+              return reply;
+            }),
+          };
+        }
+        return comment;
+      })
+    );
+  }
+};
+
+const handleCommentDownvote = async (commentId, parentId, hasDownvoted) => {
+  if (user && user._id && !hasDownvoted) {
+    await downvoteComment(commentId, user._id);
+    setComments(
+      comments.map((comment) => {
+        if (comment._id === commentId) {
+          return {
+            ...comment,
+            downvotes: [...comment.downvotes, user._id],
+            upvotes: comment.upvotes.filter((id) => id !== user._id),
+          };
+        } else if (comment._id === parentId) {
+          return {
+            ...comment,
+            replies: comment.replies.map((reply) => {
+              if (reply._id === commentId) {
+                return {
+                  ...reply,
+                  downvotes: [...reply.downvotes, user._id],
+                  upvotes: reply.upvotes.filter((id) => id !== user._id),
+                };
+              }
+              return reply;
+            }),
+          };
+        }
+        return comment;
+      })
+    );
+  }
+};
+
+
+  const renderComments = (comments, parentId = id) => {
+    return comments.filter(comment => comment.parentId === parentId).map(comment => (
+      <div key={comment._id} className="comment">
+        <div className="flex gap-1">
+          <Link to={`/${comment.author.clerkId}`}>
+            <p className="text-secondary_text text-sm mb-2 underline">
+              {comment.author.name}
+            </p>
+          </Link>
+          <p className="text-secondary_text text-sm mb-2">
+            {formatDate(comment.date)}
+          </p>
+        </div>
+        <p className="text-primary_text">{comment.content}</p>
+        <div className="flex items-center text-secondary_text mt-2">
+          <span className="flex items-center mr-4">
+            <FiArrowUp
+              className={`mr-2 ${comment.upvotes.includes(user?._id) ? "text-primary" : ""}`}
+              onClick={() => handleCommentUpvote(comment._id, comment.parentId, comment.upvotes.includes(user?._id))}
+            />
+            {comment.upvotes.length}
+          </span>
+          <span className="flex items-center mr-4">
+            <FiArrowDown
+              className={`mr-2 ${comment.downvotes.includes(user?._id) ? "text-primary" : ""}`}
+              onClick={() => handleCommentDownvote(comment._id, comment.parentId, comment.downvotes.includes(user?._id))}
+            />
+            {comment.downvotes.length}
+          </span>
+          <button
+            onClick={()=> handleReplyClick(comment)}
+            className="text-primary underline"
+          >
+            Reply
+          </button>
+        </div>
+        <div className="ml-8">
+          {comment.parentId === id && renderComments(comment.replies, comment._id)}
+          {replyingTo === comment._id && (
+            <form onSubmit={(e) => handleReplySubmit(e, comment.parentId === id ? comment._id : comment.parentId)} className="mt-4">
+              <textarea
+                value={replyContent}
+                onChange={(e) => setReplyContent(e.target.value)}
+                className="w-full p-2 border rounded-lg"
+                rows="2"
+                placeholder="Add a reply..."
+                required
+              />
+              <button
+                type="submit"
+                className="mt-2 bg-primary text-primary_text hover:bg-border hover:text-primary px-4 py-2 rounded-lg"
+              >
+                Add Reply
+              </button>
+            </form>
+          )}
+        </div>
+      </div>
+    ));
+  };
+
   return (
     <>
       <div className="w-full flex justify-center bg-background drop-shadow-2xl">
@@ -119,10 +291,7 @@ const BlogDetailPage = () => {
       <div className="min-h-screen bg-background text-primary_text p-4 flex flex-col items-center">
         <div className="w-full lg:w-2/3 bg-background p-4 rounded-lg shadow-lg">
           <div className="mt-4 self-start flex flex-wrap items-center justify-between">
-            <Link
-              to="/dashboard"
-              className="text-primary underline flex items-center"
-            >
+            <Link to="/dashboard" className="text-primary underline flex items-center">
               <IoArrowBack className="text-primary " />
               Back to Dashboard
             </Link>
@@ -137,9 +306,7 @@ const BlogDetailPage = () => {
           <h1 className="text-3xl font-bold mb-4">{blogPost.title}</h1>
           <div className="flex items-center text-secondary_text mb-4">
             <p className="text-sm">by {blogPost.author.name}</p>
-            <p className="text-sm ml-4">
-              {formatDate(blogPost.date)}
-            </p>
+            <p className="text-sm ml-4">{formatDate(blogPost.date)}</p>
           </div>
           <p className="text-primary mb-4">
             Problem Link:{" "}
@@ -159,10 +326,18 @@ const BlogDetailPage = () => {
           </div>
           <div className="flex items-center text-secondary_text mb-4">
             <span className="flex items-center mr-4">
-              <FiThumbsUp className="mr-2" /> {upvoteCount}
+              <FiArrowUp
+                className={`mr-2 ${hasUpvoted ? "text-primary" : ""}`}
+                onClick={handleUpvote}
+              />
+              {upvoteCount}
             </span>
             <span className="flex items-center mr-4">
-              <FaThumbsDown className="mr-2" /> {downvoteCount}
+              <FiArrowDown
+                className={`mr-2 ${hasDownvoted ? "text-primary" : ""}`}
+                onClick={handleDownvote}
+              />
+              {downvoteCount}
             </span>
             <span className="flex items-center mr-4">
               <FiMessageCircle className="mr-2" /> {comments.length}
@@ -170,76 +345,52 @@ const BlogDetailPage = () => {
             <span className="flex items-center mr-4">
               <FaEye className="mr-2" /> {viewCount}
             </span>
+            <span className="flex items-center mr-4">
+              <FiBookmark
+                className={`mr-2 ${isFavorite ? "text-primary" : ""}`}
+                onClick={isFavorite ? handleUnfavorite : handleFavorite}
+              />
+              {isFavorite ? "Unfavorite" : "Favorite"}
+            </span>
           </div>
           <div className="mb-8">
             <p>{blogPost.content}</p>
           </div>
+          <div className="flex items-center justify-between mb-4">
+            <h2 className="text-2xl font-bold">Comments</h2>
+            <div className="flex items-center space-x-2">
+              <select
+                className="select select-bordered w-full lg:w-auto"
+                value={sortMethod}
+                onChange={(e) => setSortMethod(e.target.value)}
+              >
+                <option value="recent">Recent</option>
+                <option value="oldest">Oldest</option>
+                <option value="popularity">Popularity</option>
+              </select>
+            </div>
+          </div>
           {userId && (
-            <div className="flex space-x-4">
+            <form onSubmit={handleCommentSubmit} className="mt-4">
+              <textarea
+                value={commentContent}
+                onChange={(e) => setCommentContent(e.target.value)}
+                className="w-full p-2 border rounded-lg"
+                rows="4"
+                placeholder="Add a comment..."
+                required
+              />
               <button
-                onClick={handleUpvote}
-                className={`flex items-center p-2 rounded-lg ${
-                  hasUpvoted
-                    ? "bg-green-500 text-white"
-                    : "bg-gray-200 text-gray-700"
-                }`}
+                type="submit"
+                className="mt-2 bg-primary text-primary_text hover:bg-border hover:text-primary px-4 py-2 rounded-lg"
               >
-                {hasUpvoted ? <TiArrowUp /> : <TiArrowUpOutline />} Upvote
+                Add Comment
               </button>
-              <button
-                onClick={handleDownvote}
-                className={`flex items-center p-2 rounded-lg ${
-                  hasDownvoted
-                    ? "bg-red-500 text-white"
-                    : "bg-gray-200 text-gray-700"
-                }`}
-              >
-                {hasDownvoted ? <TiArrowDown /> : <TiArrowDownOutline />}{" "}
-                Downvote
-              </button>
-            </div>
+            </form>
           )}
-          <section className="mt-8">
-            <h2 className="text-2xl font-bold mb-4">Comments</h2>
-            <div className="space-y-4">
-              {comments.map((comment, index) => (
-                <div
-                  key={index}
-                  className="bg-background p-4 rounded-lg shadow border border-secondary/80"
-                >
-                  <div className="flex gap-1">
-                    <Link to={`/${comment.author.clerkId}`}>
-                      <p className="text-secondary_text text-sm mb-2 underline">
-                        {comment.author.name}
-                      </p>
-                    </Link>
-                    <p className="text-secondary_text text-sm mb-2">
-                      {formatDate(comment.date)}
-                    </p>
-                  </div>
-                  <p className="text-primary_text">{comment.content}</p>
-                </div>
-              ))}
-            </div>
-            {userId && (
-              <form onSubmit={handleCommentSubmit} className="mt-4">
-                <textarea
-                  value={commentContent}
-                  onChange={(e) => setCommentContent(e.target.value)}
-                  className="w-full p-2 border rounded-lg"
-                  rows="4"
-                  placeholder="Add a comment..."
-                  required
-                />
-                <button
-                  type="submit"
-                  className="mt-2 bg-primary text-primary_text hover:bg-border hover:text-primary px-4 py-2 rounded-lg"
-                >
-                  Add Comment
-                </button>
-              </form>
-            )}
-          </section>
+          <div className="space-y-4">
+            {renderComments(comments)}
+          </div>
         </div>
       </div>
     </>
